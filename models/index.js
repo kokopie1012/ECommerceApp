d4234403412727eb3481103d82cfba93af6b6d7b
kokopie1012 <ytube.che@gmail.com>
2022-06-04 19:58:52 +0530

    writing models

---
+++ models/index.js
@@ -0,0 +1,28 @@
+/*
+* This file will be used for the following purpose
+* 1. Create the DB connection with the help of sequelize
+* 2. Export all the functionalities of the model through this file.
+* 3. One of the advantages of using an index.js file is that other files 
+*   trying to import this file only need to provide the module name.
+*/ 
+
+const config = require("../configs/db.configs");
+const Sequelize  = require("sequelize");
+
+// Creating the db connection
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect
+    }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.category = require("./category.model");
+
+module.exports = db;
